7719c002bb1e198c7bd087d7d8718cd36f8c12eb
Will Jacques <willy321@hotmail.com>
2020-11-18 14:44:47 -0500

    gitaded

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.14.1"
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
 set :application, "William_Jacques_Conso"
-set :repo_url, "https://github.com/WillJacques/Rocket-Elevator-Foundation"
+set :repo_url, "https://github.com/WillJacques/Rocket-Elevator-Foundation.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
