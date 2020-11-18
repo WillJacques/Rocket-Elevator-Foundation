5285a09657ecd894122cea5549f5301bb1716f5f
Will Jacques <willy321@hotmail.com>
2020-11-18 16:52:26 -0500

    changefolder

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "~> 3.14.1"
 
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
-set :application, "William_Jacques_Conso"
+set :application, "WilliamJacques"
 set :repo_url, "git@github.com:WillJacques/Rocket-Elevator-Foundation.git"
 
 # Default branch is :master
