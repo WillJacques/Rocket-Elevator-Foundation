50c074836a1bf8c5ca9d186c34674f994819bcfe
Will Jacques <willy321@hotmail.com>
2020-11-20 09:14:40 -0500

    skipbeforeaction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+    skip_before_action :verify_authenticity_token
+
     def access_denied(exception)
         redirect_to index_path, alert: exception.message
     end
