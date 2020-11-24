13a3d98498f2b8acffb8fa4d2f56501b1502f54b
Will Jacques <willy321@hotmail.com>
2020-11-24 06:24:47 -0500

    2variables_method

--- lib/elevator_media.rb
+++ lib/elevator_media.rb
@@ -1,26 +1,29 @@
 module ElevatorMedia
     class Streamer
-        def self.getContent(input)
+        def self.getContent(status, province)
             require 'uri'
             require 'net/http'
             require 'openssl'
             require 'json'
 
-            url = URI("https://covid1910.p.rapidapi.com/data/#{input}/country/canada/province/alberta")
+            url = URI("https://covid1910.p.rapidapi.com/data/#{status}/country/canada/province/#{province}")
 
             http = Net::HTTP.new(url.host, url.port)
             http.use_ssl = true
             http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
             request = Net::HTTP::Get.new(url)
-            request["x-rapidapi-key"] = '0df282d5dbmsh02fede9da7f3a2bp1f9b65jsnabd892a3474b'
+            request["x-rapidapi-key"] = 
             request["x-rapidapi-host"] = 'covid1910.p.rapidapi.com'
 
             response = http.request(request)
             puts response.read_body
             parsed_json = JSON.parse(response.body)
-            puts parsed_json[0]["#{input}"]
-            return parsed_json[0]["#{input}"]
+            number_of_case = parsed_json[0]["#{status}"]
+            puts number_of_case
+            integrated_text = "#{province} has #{number_of_case} #{status} cases"
+            puts integrated_text
+            return integrated_text
         end
     end
 end
--- spec/elevator_media_spec.rb
+++ spec/elevator_media_spec.rb
@@ -3,11 +3,11 @@ require 'elevator_media'
 describe ElevatorMedia::Streamer do
     describe "getContent" do 
         context "Return all stats" do
-            it 'returns integer on confirmed from alberta' do
-                expect(ElevatorMedia::Streamer.getContent("confirmed")).to be_kind_of(Integer) 
+            it 'returns integer on status for a province' do
+                expect(ElevatorMedia::Streamer.getContent("confirmed","quebec")).to be_kind_of(String) 
             end 
-            it 'returns integer on death from alberta' do
-                expect(ElevatorMedia::Streamer.getContent("death")).to be_kind_of(Integer) 
+            it 'returns integer on status for a province' do
+                expect(ElevatorMedia::Streamer.getContent("death", "quebec")).to be_kind_of(String) 
             end 
         end
     end
