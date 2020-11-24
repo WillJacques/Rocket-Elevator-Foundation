8b27d00dda70a74da3259d1e75c923ad491401b0
Will Jacques <willy321@hotmail.com>
2020-11-24 18:47:24 -0500

    all3testsdone

--- spec/elevator_media_spec.rb
+++ spec/elevator_media_spec.rb
@@ -5,7 +5,7 @@ describe ElevatorMedia::Streamer do
     describe "getContent" do 
         context "Return Quebec Covid Count" do
             it 'returns integrated string on status for a province' do
-                expect(ElevatorMedia::Streamer.getContent("confirmed","alberta")).to be_kind_of(String) 
+                expect(ElevatorMedia::Streamer.getContent("confirmed","quebec")).to be_kind_of(String) 
             end 
             it 'returns "Please select all infos required" if one is not selected' do
                 expect(ElevatorMedia::Streamer.getContent("death", "")).to eq("<h2>Please select all infos required</h2>") 
@@ -33,22 +33,39 @@ describe ElevatorMedia::Streamer do
         end
     end
 
-    #describe "get_covid_stats_from_web" do
-
-        # it "should http the right service" do
-        #     net_http = double('net_hhtp')
-        #     expect(Net::HTTP).to receive(:new).with("covid1910.p.rapidapi.com", 443) {net_http}
-        #     expect(net_http).to receive(:use_ssl=).with(true)
-        #     expect(net_http).to receive(:verify_mode=).with(OpenSSL::SSL::VERIFY_NONE)
-        #     service_response = double('service_response')
-        #     expect(Net::HTTP::Get).to receive(:new).with(URI("https://covid1910.p.rapidapi.com/data/whatever/country/canada/province/quebec")) {service_response}
-        #     ElevatorMedia::Streamer.get_covid_stats_from_web('whatever', 'quebec')
-        # end
-
-        # require 'webmock/rspec'
-        # WebMock.disable_net_connect!(allow_localhost: true)
-
-    #end    
-
+    # describe "get_covid_stats_from_web" do
+    #     it "should http the right service" do
+    #         net_http = double('net_hhtp')
+    #         expect(Net::HTTP).to receive(:new).with("covid1910.p.rapidapi.com", 443) {net_http}
+    #         expect(net_http).to receive(:use_ssl=).with(true)
+    #         expect(net_http).to receive(:verify_mode=).with(OpenSSL::SSL::VERIFY_NONE)
+    #         service_response = double('service_response')
+    #         expect(Net::HTTP::Get).to receive(:new).with(URI("https://covid1910.p.rapidapi.com/data/whatever/country/canada/province/quebec")) {service_response}
+    #         ElevatorMedia::Streamer.get_covid_stats_from_web('whatever', 'quebec')
+    #     end
+    # end    
 end
 
+RSpec.describe LeadsController do
+    describe "search in lead ID #1" do
+        it 'should get phone number from first lead' do
+            expect(Lead.find(1)[:phone]).to eq('1-732-667-6753') 
+        end
+    end
+end
+
+RSpec.describe QuotesController, :type => :controller do
+    describe "GET new_quote" do
+        it "has a 200 status code" do
+            get :new
+            expect(response.status).to eq(200)
+        end
+    end
+end
+
+RSpec.describe "routes for Interventions", :type => :routing do
+    it "routes /intervention to the intervention controller" do
+        expect(get("/intervention")).
+            to route_to("interventions#new")
+    end
+end
